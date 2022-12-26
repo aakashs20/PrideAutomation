b8e37fbb764f338427ab23b63847c9bc297993d1
Ketan Tank <ketan.tank@xoriant.com>
2022-12-26 16:27:59 +0530

    Added path for the delivery milestone

--- src/test/java/com/project/pTracker/Utils/Constants.java
+++ src/test/java/com/project/pTracker/Utils/Constants.java
@@ -13,6 +13,7 @@ public class Constants extends TestBase {
 	public static String workspace = System.getProperty("user.dir");
 	public static String configPath = workspace+"\\src\\test\\resources\\config.properties";
 	public static String configFile = workspace+"\\src\\test\\resources\\constants.properties";
+	public static String deliveryMilestonePropertyFile = workspace+"\\src\\test\\resources\\deliveryMilestone.properties";
 	public static String datapoolPath = workspace+"\\test-data-files\\UI-TestData\\TC_CreateFixedPriceNewProject.xls";
 	public static String endtoenddatapoolPath = workspace+"\\test-data-files\\UI-TestData\\TC_END_TO_END_FIXED_PRICE_PROJECT.xls";
 	public static String sheetName = "Automation";
